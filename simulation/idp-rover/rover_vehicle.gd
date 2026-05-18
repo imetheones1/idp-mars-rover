3b71dc4b2835be9881133545781ae41555b2d2df
Magic Man <65839659+imetheones1@users.noreply.github.com>
2026-05-18 16:50:35 -0700

    delete that

--- simulation/idp-rover/rover_vehicle.gd
+++
@@ -1,9 +0,0 @@
-extends VehicleBody3D
-
-@export var max_steer := 0.9
-@export var engine_power := 100
-
-func _physics_process(delta: float):
-	steering = move_toward(steering,Input.get_axis("right","left") * max_steer, delta * 10)
-	engine_force = Input.get_axis("backward","forward") * engine_power
-	#engine_force = engine_power
